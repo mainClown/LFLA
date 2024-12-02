b3d383a98c0c4bef2d31d80ad4abc8880aec60be
everslay <150055439+mainClown@users.noreply.github.com>
2024-12-02 23:30:47 +0300

    см описание
    Для тестирования кода нужно настроить сцены и привязать к предметам скрипты, спрайты

--- UnityProject/Assets/Scripts/Item.cs
+++ UnityProject/Assets/Scripts/Item.cs
@@ -1,20 +1,62 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Item : MonoBehaviour
+public class ItemObject : MonoBehaviour
 {
-    public int ItemID;
-    public GameObject ItemObject;
-    public void HideItem()
+    /* (Вроде бы)
+     * Для работы скрипта на сцене нужно разместить объект EventSystem(находится во вкладке UI)
+     * В камере разместить компонент Physics 2D Raycaster
+     */
+    public string Name;
+    public int ItemId;
+    public bool Necessary;
+    public Sprite HighliteFileName; // Подсвеченная текстура
+    public Sprite NoHighliteFileName; // Обычная текстура
+    private SpriteRenderer spriteRenderer;
+    public Text nameDisplay; // Ссылка на UI текст для отображения имени
+
+    // Start is called before the first frame update
+    void Start()
     {
-        Inventory.Instance.AddItem(this);
-        ItemObject.SetActive(false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Устанавка исходной текстуры
+        if(spriteRenderer == null)
+        {
+            spriteRenderer.sprite = NoHighlightSprite;
+        }   
     }
-    public void StartMiniGame(string MiniGameName) 
+
+    private void OnMouseEnter()
     {
-        SceneManager.LoadScene(MiniGameName);
+        HighliteItem(ItemId);
+        nameDisplay.text = Name; // Установка текста
+        nameDisplay.gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.sprite = NoHighlightSprite;
+        nameDisplay.gameObject.SetActive(false); // Скрытие текста
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Скрывает предмет и вызывает AddItem(int ItemId) для добавления этого предмета в инвентарь.
+    void HideItem(int ItemId)
+    {
+        GameObject.SetActive(false);
+        Inventory.AddItem(ItemId);
+    }
+
+    // Меняет текстуру предмета на подсвеченный вариант при наведении на него мышкой.
+    void HighliteItem(int ItemId)
+    {
+        spriteRenderer.sprite = HighlightSprite;
     }
 }
--- UnityProject/Assets/Scripts/MiniGameItem.cs
+++ UnityProject/Assets/Scripts/MiniGameItem.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinigameItem : MonoBehaviour
 {
-    public int MGItemID;
-    public bool IsGood;
-    public GameObject MGItem;
-    //To do// ћб к чЄрту эту мини-игру на кухне?
-    public void HideItem()
+    public int Id;
+    public string Name;
+    public bool IsGood = false;
+    public Text nameDisplay; // —сылка на UI текст дл€ отображени€ имени
+
+    public void HideItem(int Id)
     {
-        Destroy(MGItem);
+        GameObject.SetActive(false);
     }
 }
